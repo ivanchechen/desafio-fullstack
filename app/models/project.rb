7f9907875db2b42dc86753f6b4facc19b419529f
Danielle de Fátima Ivanchechen <danielle_de_fatima@hotmail.com>
2020-11-10 17:31:15 -0300

    Update project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,28 +1,20 @@
 class Project < ApplicationRecord
     has_many :activities
 
-
     def late
-        date_now = Date.today
-        if date_now > self.end_date
-            return "Sim"
-        else
-            return "Não"
-        end
+        Date.today > end_date ? "Sim" : "Não"
     end 
+
     def complete
         finished = 0.0
         qtd_activities = self.activities.count
-        self.activities.each do |activity| 
+        activities.each do |activity| 
             if activity.finished  
                 finished = finished + 1
             end
         end
-        if qtd_activities != 0
-            return "#{((finished / qtd_activities) * 100).round }%"
-        else
-            return "Sem atividades"
-        end
-        
+
+        qtd_activities != 0 ? "#{((finished / qtd_activities) * 100).round }%" : "Sem atividades"
+      
     end 
 end
