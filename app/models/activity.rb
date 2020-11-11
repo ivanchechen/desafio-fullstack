9c7de9cecf7397fb9ee54bfd2129c82f475f31ac
Danielle de Fátima Ivanchechen <danielle_de_fatima@hotmail.com>
2020-11-11 10:31:28 -0300

    Add Campos obrigatórios

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,8 @@
 class Activity < ApplicationRecord
   belongs_to :project
+
+  validates  :id_project , :presence => { :message => "Id do projeto é obrigatório !"}
+  validates  :name , :presence => { :message => "Nome da atividade é obrigatório !"}
+  validates  :init_date , :presence => { :message => "Data de início da atividade é obrigatório !"}
+  validates  :end_date , :presence => { :message => "Date de fim da atividade é obrigatório !"}
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord
     has_many :activities
-
+    validates  :name , :presence => { :message => "Nome do projeto é obrigatório !"}
+    validates  :init_date , :presence => { :message => "Data de início do projeto é obrigatório !"}
+    validates  :end_date , :presence => { :message => "Data de fim do projeto é obrigatório !"}
     def late
         Date.today > end_date ? "Sim" : "Não"
     end 
@@ -13,8 +15,6 @@ class Project < ApplicationRecord
                 finished = finished + 1
             end
         end
-
         qtd_activities != 0 ? "#{((finished / qtd_activities) * 100).round }%" : "Sem atividades"
-      
     end 
 end
